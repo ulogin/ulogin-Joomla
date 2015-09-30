02637299501e417d24546198a0001d06e1d042b3
chernozor <chernozor@yandex.ru>
2015-09-30 18:53:11 +0300

    Joomla 2.5

--- administrator/components/com_ulogin/sql/uninstall.mysql.utf8.sql
+++
@@ -1,1 +0,0 @@
--- DROP TABLE IF EXISTS `#__ulogin_users`;
--- administrator/components/com_ulogin/ulogin.php
+++
@@ -1,6 +0,0 @@
-<?php
-// Запрет прямого доступа.
-defined('_JEXEC') or die;
-
-$app = JFactory::getApplication();
-$app->redirect('administrator/index.php?option=com_config&view=component&component=com_ulogin');
